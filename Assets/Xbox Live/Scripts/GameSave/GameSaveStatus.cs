ae306506c52c6f3475ba14e1c4b412f44cc082e4
MSFT-Heba <heba@microsoft.com>
2017-05-04 10:30:02 -0700

    GameSavePlugin Scripts and Packaging (#28)
    - Added Scripts that surface GameSaves in Unity
    - Added the generation of the GameSave Plugin within BuildScript.ps1

---
+++ Assets/Xbox Live/Scripts/GameSave/GameSaveStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Xbox.Services.ConnectedStorage
+{
+    public enum GameSaveStatus
+    {
+        Abort,
+        InvalidContainerName,
+        NoAccess,
+        OutOfLocalStorage,
+        UserCanceled,
+        UpdateTooBig,
+        QuotaExceeded,
+        ProvidedBufferTooSmall,
+        BlobNotFound,
+        NoXboxLiveInfo,
+        ContainerNotInSync,
+        ContainerSyncFailed,
+        UserHasNoXboxLiveInfo,
+        ObjectExpired,
+        Ok
+    }
+}
+
---
+++ Assets/Xbox Live/Scripts/GameSave/StorageContainerQueryResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Xbox.Services.ConnectedStorage
+{
+    using global::System.Collections.Generic;
+
+    public class StorageContainerQueryResult
+    {
+        public GameSaveStatus Status { get; set; }
+
+        public List<StorageContainerInfo> Value { get; set; }
+    }
+}
