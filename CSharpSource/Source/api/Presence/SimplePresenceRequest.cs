2923e359f2a76f466c2b954d9757c99fbbc66339
MSFT-heba <heba@microsoft.com>
2017-07-13 15:55:06 -0700

    Adding forgotten new class

---
+++ CSharpSource/Source/api/Presence/SimplePresenceRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Xbox.Services.Shared.Presence
+{
+    public class SimplePresenceRequest
+    {
+        public string State { get; set; }
+    }
+}
