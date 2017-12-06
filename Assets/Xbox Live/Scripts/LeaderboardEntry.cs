55be3e26f431bd4f92d8addfb93f45c5f0e0eee8
Ramsey <31288345+rakhad@users.noreply.github.com>
2017-12-05 16:42:38 -0800

    Leaderboard prefab fixes

--- Assets/Xbox Live/Scripts/LeaderboardEntry.cs
+++ Assets/Xbox Live/Scripts/LeaderboardEntry.cs
@@ -2,7 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // 
 using Microsoft.Xbox.Services.Leaderboard;
-
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,6 +29,51 @@ public class LeaderboardEntry : MonoBehaviour
             this.gamertagText.text = this.data.Gamertag;
             this.rankText.text = this.data.Rank.ToString();
             this.valueText.text = this.data.Values[0];
+            this.gamerpicImage.sprite = null;
+        }
+    }
+
+    string gamerpicUrl;
+    public string GamerpicUrl
+    {
+        get
+        {
+            return this.gamerpicUrl;
+        }
+        set
+        {
+            this.gamerpicUrl = value;
+            StartCoroutine(LoadGamerpic());
+        }
+    }
+    IEnumerator LoadGamerpic()
+    {
+        var www = new WWW(gamerpicUrl);
+        yield return null;
+
+        while (!www.isDone)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        try
+        {
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                throw new Exception(www.error);
+            }
+                
+            var t = www.texture;
+            var r = new Rect(0, 0, t.width, t.height);
+            this.gamerpicImage.color = Color.white;
+            this.gamerpicImage.sprite = Sprite.Create(t, r, Vector2.zero);
+        }
+        catch (Exception ex)
+        {
+            if (XboxLiveServicesSettings.Instance.DebugLogsOn)
+            {
+                Debug.Log("There was an error while loading " + gamertagText.text + "'s gamerpic. Exception: " + ex.Message);
+            }
         }
     }
 }
