4a40414895b223afe7c585e745b2d3f047f3ed83
Ramsey Khadder <rakhad@microsoft.com>
2017-12-08 14:38:20 -0800

    Added a ui component for device connectivity

---
+++ Assets/Xbox Live/Scripts/AvailabilityCheck.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// 
+using UnityEngine;
+
+public class AvailabilityCheck : MonoBehaviour {
+    // The UI element to display when the device is offline
+    public GameObject OfflineStatus;
+
+    bool showingOffline;
+
+    void Update()
+    {
+        bool offline = Application.internetReachability == NetworkReachability.NotReachable;
+        if (showingOffline != offline)
+        {
+            OfflineStatus.SetActive(offline);
+            showingOffline = offline;
+        }
+    }
+}
