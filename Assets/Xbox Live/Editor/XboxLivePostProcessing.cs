65816bca26a953b14a3e68b2813193376e958d2f
Marcelo Chaves de Oliveira <marcelochaves17@gmail.com>
2018-09-15 13:25:21 -0300

    Improvements to Editor scripts

--- Assets/Xbox Live/Editor/XboxLivePostProcessing.cs
+++ Assets/Xbox Live/Editor/XboxLivePostProcessing.cs
@@ -1,8 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // 
-namespace Assets.Xbox_Live.Editor
-{
+namespace Assets.Xbox_Live.Editor {
     using System;
     using System.IO;
     using System.Xml;
@@ -23,10 +22,9 @@ namespace Assets.Xbox_Live.Editor
     /// association and including Xbox Live configuration files.
     /// </summary>
     [InitializeOnLoad]
-    public class XboxLivePostProcessing
-    {
-        static XboxLivePostProcessing()
-        {
+    public class XboxLivePostProcessing {
+
+        static XboxLivePostProcessing () {
             ProjectFilesGenerator.ProjectFileGeneration += AddXboxServicesConfig;
         }
 
@@ -40,10 +38,8 @@ namespace Assets.Xbox_Live.Editor
         /// This only modifies the Unity debug projects and will not have any
         /// effect on projects built as part of the Unity UWP build process.
         /// </remarks>
-        private static string AddXboxServicesConfig(string fileName, string fileContent)
-        {
-            if (!fileName.EndsWith(".Editor.csproj"))
-            {
+        private static string AddXboxServicesConfig (string fileName, string fileContent) {
+            if (!fileName.EndsWith(".Editor.csproj")) {
                 const string configFileElement =
                     "    <Content Include=\"" + XboxLiveAppConfiguration.FileName + "\">\r\n" +
                     "       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>\r\n" +
@@ -58,15 +54,13 @@ namespace Assets.Xbox_Live.Editor
         }
 
         [PostProcessBuild(1)]
-        public static void OnPostprocessBuild(BuildTarget target, string solutionFolder)
-        {
+        public static void OnPostprocessBuild (BuildTarget target, string solutionFolder) {
+            
             // When we're building for UWP we need to do some extra stuff.
-            if (target == BuildTarget.WSAPlayer)
-            {
+            if (target == BuildTarget.WSAPlayer) {
                 XboxLiveConfigurationEditor editorWindow = EditorWindow.GetWindow<XboxLiveConfigurationEditor>("Xbox Live");
 
-                if (editorWindow.configuration == null || editorWindow.configuration.TitleId == 0)
-                {
+                if (editorWindow.configuration == null || editorWindow.configuration.TitleId == 0) {
                     Debug.LogWarning("Unable to find a valid Xbox Live configuration file so Xbox Live will not be enabled on UWP project.\r\nOpen \"Xbox Live > Configuration\" and click \"Run Xbox Live Association Wizard\" to configure for Xbox Live access.");
                     return;
                 }
@@ -76,28 +70,23 @@ namespace Assets.Xbox_Live.Editor
             }
         }
 
-        private static void CopyConfigurationFile(string filename, string projectFolder)
-        {
+        private static void CopyConfigurationFile (string filename, string projectFolder) {
             File.Copy(
                 Path.Combine(Application.dataPath, "../" + filename),
                 Path.Combine(projectFolder, filename),
                 true);
         }
 
-        private static void UpdateProjectFile(string projectFolder, XboxLiveAppConfiguration configuration)
-        {
+        private static void UpdateProjectFile(string projectFolder, XboxLiveAppConfiguration configuration) {
             var scriptingBackend = PlayerSettings.GetScriptingBackend(BuildTargetGroup.WSA);
 
             // Copy the XboxServices.config file over to the project folder.
             CopyConfigurationFile(XboxLiveAppConfiguration.FileName, projectFolder);
 
             string projectFile = null;
-            if (scriptingBackend == ScriptingImplementation.WinRTDotNET)
-            {
+            if (scriptingBackend == ScriptingImplementation.WinRTDotNET) {
                 projectFile = Path.Combine(projectFolder, Application.productName + ".csproj");
-            }
-            else if (scriptingBackend == ScriptingImplementation.IL2CPP)
-            {
+            } else if (scriptingBackend == ScriptingImplementation.IL2CPP) {
                 projectFile = Path.Combine(projectFolder, Application.productName + ".vcxproj");
             }
 
@@ -109,19 +98,14 @@ namespace Assets.Xbox_Live.Editor
             // Add the XboxService.config to the project if it doesn't exist already.
             XElement identityItemGroup = project.XPathSelectElement("msb:Project/msb:ItemGroup[msb:AppxManifest]", ns);
 
-            if (scriptingBackend == ScriptingImplementation.WinRTDotNET)
-            {
-                if (identityItemGroup.XPathSelectElement("msb:Content[@Include='XboxServices.config']", ns) == null)
-                {
+            if (scriptingBackend == ScriptingImplementation.WinRTDotNET) {
+                if (identityItemGroup.XPathSelectElement("msb:Content[@Include='XboxServices.config']", ns) == null) {
                     identityItemGroup.Add(new XElement(msb + "Content",
                         new XAttribute("Include", "XboxServices.config"),
                         new XElement(msb + "CopyToOutputDirectory", "PreserveNewest")));
                 }
-            }
-            else if (scriptingBackend == ScriptingImplementation.IL2CPP)
-            {
-                if (identityItemGroup.XPathSelectElement("msb:None[@Include='XboxServices.config']", ns) == null)
-                {
+            } else if (scriptingBackend == ScriptingImplementation.IL2CPP) {
+                if (identityItemGroup.XPathSelectElement("msb:None[@Include='XboxServices.config']", ns) == null) {
                     identityItemGroup.Add(new XElement(msb + "None",
                         new XAttribute("Include", "XboxServices.config"),
                         new XElement(msb + "DeploymentContent", "true")));
